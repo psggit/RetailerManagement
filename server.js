1fda4976683a2be4e05ddb0d48bb44fe6c38ceda
Madhur Garg <madhur@hipbar.com>
2018-12-18 01:41:21 +0530

    changes

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.get('*.js', function (req, res, next) {
   // const runtimeUrlRegex = /runtime.*.js/
   const vendorUrlRegex = /vendor.*.js/
-  // if(!runtimeUrlRegex.test(req.url)) {
-  //    req.url = req.url + '.gz';
-  //   res.set('Content-Encoding', 'gzip');
-  //   res.set('Content-Type', 'text/javascript');
-  // } 
+  if(!runtimeUrlRegex.test(req.url)) {
+     req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'text/javascript');
+  } 
   if (vendorUrlRegex.test(req.url)) {
     res.setHeader('Cache-Control', 'private, max-age=31536000')
   }
