7e01de29a00115acee5e0664f27a28b6bb1a198e
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2018-12-17 22:54:42 +0530

    Fix in publicpath

--- webpack.common.js
+++ webpack.common.js
@@ -61,7 +61,7 @@ module.exports = {
   output: {
     filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/admin'
+    publicPath: '/'
   },
   optimization: {
     splitChunks: {
