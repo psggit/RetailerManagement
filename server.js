d8e8824aedc054b57dc88e43061b5d5f985686d4
Madhur Garg <madhur@hipbar.com>
2019-05-14 22:03:52 +0530

    added react helmet for security headers

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const path = require('path')
 const app = express();
 // const ASSET_PATH = process.env.ASSET_PATH || '/'
 
+app.use(helmet({
+  frameguard: {
+    action: "deny"
+  }
+}))
+
 app.get('*.js', function (req, res, next) {
   const runtimeUrlRegex = /runtime.*.js/
   const vendorUrlRegex = /vendor.*.js/
-  if(!runtimeUrlRegex.test(req.url)) {
-     req.url = req.url + '.gz';
+  if (!runtimeUrlRegex.test(req.url)) {
+    req.url = req.url + '.gz';
     res.set('Content-Encoding', 'gzip');
     res.set('Content-Type', 'text/javascript');
-  } 
+  }
   if (vendorUrlRegex.test(req.url)) {
     res.setHeader('Cache-Control', 'private, max-age=31536000')
   }
@@ -19,7 +25,7 @@ app.get('*.js', function (req, res, next) {
 
 app.use('/admin', express.static(path.join(__dirname, 'dist')))
 
-app.get('/*', (req, res)=>{
+app.get('/*', (req, res) => {
   //console.log(req.query);
   res.sendFile(path.join(__dirname, 'dist/index.html'), (err) => {
     if (err) {
