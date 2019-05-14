f76191af43f74d9499a8b1a50c95a2e4e77dde60
Madhur Garg <madhur@hipbar.com>
2019-05-14 22:11:09 +0530

    added security headers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const helmet = require("helmet")
 // const ASSET_PATH = process.env.ASSET_PATH || '/'
 
 app.use(helmet({
