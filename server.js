2c4c62334791a9cea72c0bf21a700f0175d93f6e
AparnaRada <50664973+AparnaRada@users.noreply.github.com>
2020-01-08 21:58:48 +0530

    Settlement users (#36)
    * Added settlement users
    
    * Added customer filter tag and payment mechanism on retailer creation and updation
    
    * Added upi_store_type and customer_filter_tag on onboarding retailer
    
    * Added new fields  in both retailer and org creation
    
    * debug
    
    * debug
    
    * debug

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.get('/*', (req, res) => {
 
 // Serve the files on port 3000.
 app.listen(8080, function () {
-  console.log('Example app listening on port the 8080!\n');
+  console.log('Example app listening on port 8080!\n');
 });
